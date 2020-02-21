5fdeb40211f7459d212a07f2e56e815bc34b3848
Sneha <mank@coursera-something.com>
2020-02-21 23:29:28 +0530

    Dependency Injection files

--- DI/app.js
+++ DI/app.js
@@ -5,13 +5,22 @@ angular.module('DIApp', [])
 .controller('DIController', DIController);
 
 function DIController ($scope,
-                       $filter) {
-  $scope.name = "Sneha";
+                       $filter,
+                       $injector) {
+  $scope.name = "Yaakov";
 
   $scope.upper = function () {
     var upCase = $filter('uppercase');
     $scope.name = upCase($scope.name);
   };
 
+  console.log($injector.annotate(DIController));
+}
+
+function AnnonateMe(name, job, blah) {
+  return "Blah!";
+}
+
+console.log(DIController.toString());
 
 })();
